26da6f51cce27b423085e30c0023cd6852594a64
Banura Randika <randika.banura@gmail.com>
2021-07-21 21:57:02 +0530

    Database file format change add

--- topsinhalamp3/downloader/song_reg.py
+++ topsinhalamp3/downloader/song_reg.py
@@ -1,36 +1,40 @@
 import mysql.connector
 
+
 class SongREG:
 
     def __init__(self, host, user, passwd, db):
-        self.conn = mysql.connector.connect(host=host, user=user, passwd=passwd, database = db)
+        self.conn = mysql.connector.connect(host=host, user=user, passwd=passwd, database=db)
 
     def store(self, arr):
         c = self.conn.cursor()
         c.execute(f'SELECT * FROM song WHERE source_id = {arr["source_id"]}')
         rst = c.fetchall()
         row_count = c.rowcount
-        
+
         if row_count == 0:
-            c.execute(f'INSERT INTO song(source_id, name, path, song_desc, status, genre_idgenre) VALUES({arr["source_id"]}, "{arr["song_name"]}", "{arr["path"]}", "{arr["song_desc"]}", 0, 1)')
+            c.execute(
+                f'INSERT INTO song(source_id, name, path, song_desc, status, genre_idgenre) VALUES({arr["source_id"]}, "{arr["song_name"]}", "{arr["path"]}", "{arr["song_desc"]}", 0, 1)')
             self.conn.commit()
             c.execute('SELECT idsong FROM song ORDER BY idsong DESC LIMIT 1')
             rst = c.fetchone()
             latest_song_id = rst[0]
-            
+
             c.execute(f'SELECT idartist FROM artist WHERE url = "{arr["artist_url"]}"')
             rst = c.fetchall()
             row_count = c.rowcount
-            
+
             if row_count == 0:
                 c.execute(f'INSERT INTO artist(idartist, url) VALUES(idartist, "{arr["artist_url"]}")')
                 self.conn.commit()
                 c.execute('SELECT idartist FROM artist ORDER BY idartist DESC LIMIT 1')
                 rst = c.fetchone()
                 latest_artist_id = rst[0]
-                c.execute(f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {latest_artist_id})')
+                c.execute(
+                    f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {latest_artist_id})')
                 self.conn.commit()
-                c.execute(f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({latest_song_id}, {latest_artist_id})')
+                c.execute(
+                    f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({latest_song_id}, {latest_artist_id})')
                 self.conn.commit()
 
                 return True
@@ -41,10 +45,12 @@ class SongREG:
                 rst = c.fetchall()
                 for row in rst:
                     if arr["artist_name"] != row[1]:
-                        c.execute(f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
+                        c.execute(
+                            f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
                         self.conn.commit()
-                
-                c.execute(f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({latest_song_id}, {artist_id})')
+
+                c.execute(
+                    f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({latest_song_id}, {artist_id})')
                 self.conn.commit()
 
                 return True
@@ -53,21 +59,22 @@ class SongREG:
             c.execute(f'SELECT * FROM song_has_artist WHERE song_idsong = {song_id}')
             rst = c.fetchall()
             row_count = c.rowcount
-            
+
             if row_count == 1:
                 artist_id = rst[0][1]
                 c.execute(f'SELECT * FROM artist WHERE idartist = {artist_id} AND url = "{arr["artist_url"]}"')
                 c.fetchall()
                 row_count = c.rowcount
-                
+
                 if row_count != 0:
-                    c.execute(f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
+                    c.execute(
+                        f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
                     self.conn.commit()
                 else:
                     c.execute(f'SELECT * FROM artist WHERE url = "{arr["artist_url"]}"')
                     rst = c.fetchall()
                     row_count = c.rowcount
-                    
+
                     if row_count == 0:
                         c.execute(f'INSERT INTO artist(url) VALUES("{arr["artist_url"]}")')
                         self.conn.commit()
@@ -75,27 +82,33 @@ class SongREG:
                         rst = c.fetchone()
                         artist_id = rst[0]
 
-                        c.execute(f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
+                        c.execute(
+                            f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
                         self.conn.commit()
-                        c.execute(f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {artist_id})')
-                        self.conn.commit() 
+                        c.execute(
+                            f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {artist_id})')
+                        self.conn.commit()
 
-                        return False 
+                        return False
                     else:
                         artist_id = rst[0][0]
-                        c.execute(f'SELECT * FROM artist_name WHERE artist_idartist = {artist_id} AND name = "{arr["artist_name"]}"')
+                        c.execute(
+                            f'SELECT * FROM artist_name WHERE artist_idartist = {artist_id} AND name = "{arr["artist_name"]}"')
                         c.fetchall()
                         row_count = c.rowcount
 
                         if row_count == 0:
-                            c.execute(f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
+                            c.execute(
+                                f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
                             self.conn.commit()
-                            c.execute(f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {artist_id})')
+                            c.execute(
+                                f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {artist_id})')
                             self.conn.commit()
 
                             return False
                         else:
-                            c.execute(f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {artist_id})')
+                            c.execute(
+                                f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {artist_id})')
                             self.conn.commit()
 
                             return False
@@ -119,19 +132,23 @@ class SongREG:
                         self.conn.commit()
                         c.execute('SELECT * FROM artist ORDER BY idartist DESC LIMIT 1')
                         rst = c.fetchone()
-                        
+
                         latest_artist_id = rst[0]
-                        c.execute(f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {latest_artist_id})')
+                        c.execute(
+                            f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {latest_artist_id})')
                         self.conn.commit()
-                        c.execute(f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {latest_artist_id})')
+                        c.execute(
+                            f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {latest_artist_id})')
                         self.conn.commit()
 
                         return False
                     else:
                         artist_id = rst[0]
-                        c.execute(f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
+                        c.execute(
+                            f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
                         self.conn.commit()
-                        c.execute(f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {artist_id})')
+                        c.execute(
+                            f'INSERT INTO song_has_artist(song_idsong, artist_idartist) VALUES({song_id}, {artist_id})')
                         self.conn.commit()
 
                         return False
@@ -140,18 +157,20 @@ class SongREG:
                         artist_id = row[1]
                         c.execute(f'SELECT * FROM artist WHERE idartist = "{artist_id}"')
                         rst = c.fetchone()
-                        
+
                         if arr["artist_url"] == rst[1]:
                             c.execute(f'SELECT * FROM artist_name WHERE name = "{arr["artist_name"]}"')
                             c.fetchone()
                             row_count = c.rowcount
 
                             if row_count == 0:
-                                c.execute(f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
+                                c.execute(
+                                    f'INSERT INTO artist_name(name, artist_idartist) VALUES("{arr["artist_name"]}", {artist_id})')
                                 self.conn.commit()
 
                                 return False
-                
+
+
 instance = SongREG("localhost", "root", "", "mp3_scraping")
 
 # instance.store({"source_id":1, "song_name":"song1", "artist_name":"artist1", "path":"path1", "artist_url":"https://site.com/artists/artist1-song-mp3", "song_desc":"song1_desc"})
